1d0d1492caa2d0cd19869c800fd4ff383fbd83e5
ssoto <scots4ever@gmail.com>
2014-05-17 21:06:03 +0200

    Modified csv reader to get typ eof rows

--- lisa_graph/lisa_modules/csv_reader.py
+++ lisa_graph/lisa_modules/csv_reader.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 #CSV_Reader for LISA
 
+from time import strptime
+
 import csv as ecsv
 
 class CSV:
@@ -13,12 +15,34 @@ class CSV:
         reader = ecsv.reader(ifile)
         rows_list= []
         for row in reader:
-            rows_list.append(row)        
+            rows_list.append(row)
         ifile.close()
         self.name = name
         self.description = description
         self.titles = rows_list[0]
         self.rows = rows_list[1:]
+
+    def __get_row_types(self, titles, example_row):
+        result = {}
+        for index in range(0,len(titles)):
+            result[index] = _get_type(example_row[index])
+        return result
+
+    def _get_type(self, str_to_parse):
+        try:
+            result = strptime(str_to_parse,'%d/%m/%Y')
+        except ValueError:
+            try:
+                result = int(str_to_parse)
+            except ValueError:
+                try:
+                    result = float(str_to_parse)
+                except ValueError:
+                    result = str_to_parse
+        return (type(result), result)
+
+
+
 
 if __name__ == "__main__":
     import sys
