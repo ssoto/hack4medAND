44637cf4e533be0e15688ba2bd9b9730663e2ad3
tataogg <aliciammf@gmail.com>
2014-05-17 18:24:42 +0200

    Embedded Keys

--- lisa_graph/lisa_models/table.py
+++ lisa_graph/lisa_models/table.py
@@ -1,11 +1,12 @@
 # -*- coding: UTF-8 -*-
 from mongoengine import *
+from lisa_models.key import Key_Model
 
 class Table_Model (Document):
     name = StringField()
     description = StringField()
     titles = ListField()
     rows = ListField()
-    keys = ListField()
+    keys = ListField(EmbeddedDocumentField(Key_Model))
 
 
--- lisa_graph/lisa_modules/db_middleware.py
+++ lisa_graph/lisa_modules/db_middleware.py
@@ -1,12 +1,19 @@
 # -*- coding: UTF-8 -*-
 from lisa_modules.csv_reader import CSV
 from lisa_models.table import Table_Model
-
+from lisa_models.key import Key_Model
 
 def persist_csv(csv_object, key_list=None):
-    extended_keys = csv_object.titles
+    
+    extended_keys = []
+    for title in csv_object.titles:
+        key_object = Key_Model(name=title)
+        extended_keys.append(key_object)
     if key_list:
-        extended_keys += key_list
+        for key in key_list:
+            key_object = Key_Model(name=key)
+            extended_keys.append(key_object)
+
     #TODO: REVISAR SI METER TITLES POR DEFECTO
     csv_table = Table_Model.objects.create(
         name = csv_object.name,
@@ -20,7 +27,5 @@ def persist_csv(csv_object, key_list=None):
 def get_suggested_keys(csv_table):
     pass
 
-def add_keys(key_list):
-    # nombre 
+def add_keys(name, key_list):
     pass
-
