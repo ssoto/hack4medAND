86935dc0c02858f2b256af34bd24a7217cc045ec
Oscar Ramirez <tuxskar@gmail.com>
2014-05-17 17:14:50 +0200

    First web version

--- lisa_graph/lisa_graph/urls.py
+++ lisa_graph/lisa_graph/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lisa_graph.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'lisa_search.views.index', name='index'),
+    url(r'^search$', 'lisa_search.views.search', name='search'),
+    url(r'^upload$', 'lisa_search.views.upload', name='upload'),
     url(r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
 )
--- lisa_graph/lisa_search/views.py
+++ lisa_graph/lisa_search/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 
 # Create your views here.
+def index(request):
+    return render_to_response(
+        'index.html', 
+        {}, 
+        context_instance=RequestContext(request)
+    )
+
+def search(request):
+    return render_to_response(
+        'lisa_search/search.html', 
+        {}, 
+        context_instance=RequestContext(request)
+    )
+def upload(request):
+    return render_to_response(
+        'lisa_search/upload.html', 
+        {}, 
+        context_instance=RequestContext(request)
+    )
