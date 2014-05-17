d17f19ca04e032bd8e06fcf499069296adfa1f00
tataogg <aliciammf@gmail.com>
2014-05-17 20:28:04 +0200

    Commit the testing.py

--- lisa_graph/testing.py
+++ lisa_graph/testing.py
@@ -1,15 +1,18 @@
 # -*- coding: UTF-8 -*-
 from lisa_modules.csv_reader import CSV
 from lisa_models.table import Table_Model
-from lisa_modules.db_middleware import persist_csv, add_keys
+from lisa_modules.db_middleware import persist_csv
+from lisa_modules.key_middleware import add_keys, get_all_keys,get_suggested_keys
 
 if __name__ == "__main__":
     from mongoengine import connect
     connect('project1')
     csv_object = CSV()
     csv_object.initialize('/home/alicia/hack4medAND/lisa_graph/resources/file.csv', 
-    					'NOMBRE_CSV',
-     					'CSV MOLÓN!')
+    					'andalucia temperatura',
+     					'Tabla con las temperaturas por provincia')
     persist_csv(csv_object,
                  ['key', 'andalucia'])
+    for key in get_suggested_keys('andalucia temperatura'):
+    	print key.name
     #add_keys('NOMBRE_CSV',['HOLA', 'MUNDO'])
